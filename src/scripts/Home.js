f1830aa013521d5f88a061b17e11e36d8f08da1d
Hugh Zhou <hughzhoutrt@gmail.com>
2025-04-08 14:19:42 -0400

    Refactor init project structure

---
+++ src/scripts/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h1>Welcome to the Pokemon Fun App!</h1>
+        <p>This is your homepage.</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;  // Ensure the default export is present
